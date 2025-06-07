e2e87e6aa508e8daaa1d3ccc9dd1cb28b287d44e
Tan Nguyen <ntantfs1603@gmail.com>
2025-06-07 07:10:08 +0700

    update role orderDetailRouter

--- src/routes/orderDetailRouter.js
+++ src/routes/orderDetailRouter.js
@@ -3,8 +3,9 @@ const controller = require("../controllers/orderDetailController");
 const { auth, authorize } = require("../middlewares/auth");
 const router = express.Router();
 const ALLOW_ROLE = ["manager", "admin", "staff"];
+const ALL_ROLE = ["manager", "admin", "staff", "customer"];
 
-router.post("/", auth, authorize(...ALLOW_ROLE), controller.createOrderDetail);
+router.post("/", auth, authorize(...ALL_ROLE), controller.createOrderDetail);
 router.get("/order/:orderId", auth, authorize(...ALLOW_ROLE), controller.getDetailsByOrder);
 router.get("/all", controller.getAllOrderDetails);
 router.get("/:id", auth, authorize(...ALLOW_ROLE), controller.getOrderDetailById);
